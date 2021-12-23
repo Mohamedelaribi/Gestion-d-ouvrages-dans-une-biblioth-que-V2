99863b57e5edd945e802413a8806c34f35ff9c58
Mohamedelaribi <93386582+Mohamedelaribi@users.noreply.github.com>
2021-12-23 11:18:29 +0100

    Breif_8

--- js.js
+++ js.js
@@ -1,18 +1,19 @@
 var input = document.getElementsByTagName("input")
-var select = document.getElementsByName("language") 
-var type = document.getElementsByName("type")
+var select = document.getElementById('lang')
+var type ;
 var lst = [];
 
 
 class Ouvrage{
-    constructor(titre,auteur,prix,email,date,language,type){
+    constructor(titre,auteur,prix,email,date,type,language,){
         this.titre = titre;
         this.auteur = auteur;
         this.prix = prix;
         this.email = email;
         this.date = date;
-        this.language = language;
         this.type = type;
+        this.language = language;
+     
     }
     détailOuvrage()
 {    return "L'ouvrage "+this.ouvrage+" est un "+this.type+" en langue "+this.language+" écrit par "+this.auteur+" et publié le "+this.date+" le prix de "+this.titre+" est de "+this.prix;
@@ -20,12 +21,12 @@ class Ouvrage{
 }
 var tab = document.querySelector("table>tbody");
 function add(){
-    let ouv = new Ouvrage(input[0].value,input[1].value,input[2].value,input[3].value,input[4].value,select.value,type.value);
-    console.log(ouv.détailOuvrage());
+    let ouv = new Ouvrage(input[0].value,input[1].value,input[2].value,input[3].value,input[4].value,document.querySelector("input[type='radio']:checked").value,select.value);
+    console.log(lst);
     lst.push(ouv);
     var tr=document.createElement("tr");
     tr.innerHTML=`
-    <td>${ouv.title}</td>
+    <td>${ouv.titre}</td>
     <td>${ouv.auteur}</td>
     <td>${ouv.prix}</td>
     <td>${ouv.email}</td>
